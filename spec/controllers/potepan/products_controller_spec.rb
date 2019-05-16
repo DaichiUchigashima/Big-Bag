fb77b80182dfe7e352c7b4fcaf60402c2d311322
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-05-16 22:53:36 +0900

    Modified products_controller_spec.rb and products_spec.rb

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
+  describe "#show" do
+    let(:product) { create(:base_product) }
+    before { get :show, params: { id: product.id } }
 
+    it 'has a 200 status code' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'responds successfully' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the :show template' do
+      expect(response).to render_template :show
+    end
+  end
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:product) { create(:product) }
+
+  scenario "商品詳細ページを閲覧する" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_content product.name
+    expect(page).to have_content product.display_price
+    expect(page).to have_content product.description
+  end
 end
