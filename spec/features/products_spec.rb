e5e2fd854dd8dab7b83c700cb8a1b47cb6d6ae33
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-05-20 17:03:23 +0900

    Modified product_spec.rb to limit the scope of the Rspec

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.feature "Products", type: :feature do
   let(:product) { create(:product) }
 
-  scenario "商品詳細ページを閲覧する" do
+  scenario "Show details of each information for single products" do
     visit potepan_product_path(product.id)
-    expect(page).to have_content product.name
-    expect(page).to have_content product.display_price
-    expect(page).to have_content product.description
+    within '.main-wrapper' do
+      expect(page).to have_content product.name
+      expect(page).to have_content product.display_price
+      expect(page).to have_content product.description
+    end
   end
 end
