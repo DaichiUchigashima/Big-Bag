ea0b48ab7e23269fabb6412e1b7cec64fb070228
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-05-21 12:30:45 +0900

    Delete be_successful test from the products_controller_spec.rb

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -14,10 +14,6 @@ RSpec.describe Potepan::ProductsController, type: :controller do
       expect(response).to have_http_status 200
     end
 
-    it 'responds successfully' do
-      expect(response).to be_successful
-    end
-
     it 'renders the :show template' do
       expect(response).to render_template :show
     end
