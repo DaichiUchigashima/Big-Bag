eb461166ee77f44f54ff304dacb53e3fc7944ba7
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-05-25 13:53:20 +0900

    Modify categories/_product_box.html.erb and related part of categories_controller.rb

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.includes(:root)
+    @products = @taxon.products.all.includes(master: [:default_price, :images])
   end
 end
