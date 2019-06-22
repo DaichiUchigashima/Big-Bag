a07a3bdf25f60a9c0beadeb44fab748585d225a7
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-06-22 11:00:54 +0900

    Modify the method related to related_products in the products_controller.rb and product_decorator.rb

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,6 +3,6 @@ class Potepan::ProductsController < ApplicationController
 
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.take(MAX_RELATED_PRODUCTS)
+    @related_products = @product.related_products.limit(MAX_RELATED_PRODUCTS)
   end
 end
