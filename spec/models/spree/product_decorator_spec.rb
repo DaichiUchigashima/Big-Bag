49342a6260b22b248b78ffefb2c98f6139e77c94
Daichi Uchigashima <ssch5050k@yahoo.co.jp>
2019-06-24 10:04:07 +0900

    Modify a test in the product_decorator_spec.rb

--- spec/models/spree/product_decorator_spec.rb
+++ spec/models/spree/product_decorator_spec.rb
@@ -5,12 +5,13 @@ RSpec.describe Spree::Product, type: :model do
     let!(:taxon_1) { create(:taxon) }
     let!(:taxon_2) { create(:taxon) }
     let!(:taxon_another) { create(:taxon) }
-    let!(:product_1) { create(:product, taxons: [taxon_1, taxon_2, taxon_another]) }
-    let!(:product_2) { create(:product, taxons: [taxon_1, taxon_2]) }
-    let!(:product_another) { create(:product, taxons: [taxon_1, taxon_another]) }
+    let!(:product) { create(:product, taxons: [taxon_1, taxon_2, taxon_another]) }
+    let!(:product_1) { create(:product, taxons: []) }
+    let!(:product_2) { create(:product, taxons: [taxon_1]) }
+    let!(:product_another) { create(:product, taxons: [taxon_2, taxon_another]) }
 
     it "selects a product from same category" do
-      expect(product_1.related_products).to contain_exactly(product_2, product_another)
+      expect(product.related_products).to contain_exactly(product_2, product_another)
     end
   end
 end
